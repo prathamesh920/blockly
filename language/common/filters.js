9dd42de224772a3ad553114ff7402d72613b8ff6
manojgudi <manoj.p.gudi@gmail.com>
2013-06-02 13:31:38 +0530

    added first building block, very rough

---
+++ language/common/filters.js
@@ -0,0 +1,10 @@
+Blockly.Language.math_foo = {
+  helpUrl: 'http://www.example.com/',
+  init: function() {
+    this.setColour(230);
+    .appendValueInput("Manoj");
+    .setInputsInline(true);
+    this.setOutput(true, "Number");
+    this.setTooltip('');
+  }
+};
