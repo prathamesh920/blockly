5811c52e23efb8850c5c82b242668d8694f7bd46
manojgudi <manoj.p.gudi@gmail.com>
2013-06-03 15:50:24 +0530

    added soy2js compiler

--- language/common/filters.js
+++ language/common/filters.js
@@ -2,8 +2,8 @@ Blockly.Language.math_foo = {
   helpUrl: 'http://www.example.com/',
   init: function() {
     this.setColour(230);
-    .appendValueInput("Manoj");
-    .setInputsInline(true);
+    this.appendValueInput("Manoj");
+    this.setInputsInline(true);
     this.setOutput(true, "Number");
     this.setTooltip('');
   }
