dfeb6787eafe88b9dcb6cdc2fd13a06ba5181e38
manojgudi <manoj.p.gudi@gmail.com>
2013-06-04 11:20:30 +0530

    first custom block, next: code generation

--- language/common/filters.js
+++ language/common/filters.js
@@ -1,9 +1,10 @@
-Blockly.Language.math_foo = {
+Blockly.Language.filters_hpf = {
   helpUrl: 'http://www.example.com/',
   init: function() {
-    this.setColour(256)
-    .appendValueInput("Manoj");
-    .setInputsInline(true);
+    this.setColour(290);
+    this.appendValueInput("NAME")
+        .setCheck("Number")
+        .appendTitle("My HPF");
     this.setOutput(true, "Number");
     this.setTooltip('');
   }
